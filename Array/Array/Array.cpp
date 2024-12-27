a9b228bda6734b8573586e3800ac888a8a065459
KimLink0-0 <linkedcirrus93@gmail.com>
2024-12-27 18:17:59 +0900

    Upload

--- Array/Array/Array.cpp
+++ Array/Array/Array.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 double sumArray(int n)
 {
-	
+
 	int* array = new int[n];
 
 
@@ -26,7 +26,7 @@ double sumArray(int n)
 	return sum;
 }
 
-double avgArray(int n, double sum) 
+double avgArray(int n, double sum)
 {
 
 	double avg;
@@ -38,7 +38,7 @@ double avgArray(int n, double sum)
 
 
 
-int main() 
+int main()
 {
 
 	int n;
@@ -46,7 +46,7 @@ int main()
 	cout << "원소의 갯수를 입력해주세요. : ";
 	cin >> n;
 
-	double sum = 0; 
+	double sum = 0;
 	double avg = 0;
 
 	sum = sumArray(n);
@@ -54,7 +54,7 @@ int main()
 
 	cout << "배열의 합은 :" << sum << endl;
 	cout << "배열의 평균은 :" << avg << endl;
-	
+
 
 	return 0;
 }
